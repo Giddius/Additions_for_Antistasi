9cdadfb2aad4085b228aaa8b99b8eb9fba363046
Giddius <GordonGrossmann@gmx.at>
2020-04-20 01:37:50 +0200

    paas

--- Addons/main/config.cpp
+++ Addons/main/config.cpp
@@ -8,20 +8,29 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀//
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "X_macro_variant_define.hpp"
 
-
-
-
+//nununununununununununununununununununununununununununununununnunununununununununununununununununun//
+//»»————-:<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:><:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:————-««//
+//nununununununununununununununununununununununununununununununnunununununununununununununununununun//
 
 class CfgPatches
 {
-	class MODFOLDER
+	class bdsm_AfA_main
 	{
 		units[] = {};
 		weapons[] ={};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"rhsusf_main","A3_Characters_F","A3_Data_f","A3_Weapons_F","A3_Characters_F_Proxies"};
+		requiredAddons[] = {"rhsusf_main","A3_Characters_F","A3_Data_f","A3_Weapons_F","A3_Characters_F_Proxies","bdsm_AfA_hqadd"};
 	};
 };
 
+class CfgEditorCategories
+{
+	class bdsm_AfA_all
+	{
+		displayName = "AfA Objects/Structures";
+	};
+};
 
+ #include "X_EditorSubcat_Faction_Insignia_Marker.hpp"
